e5b8660d4738b63d8c7cffdd17b9eb0378df67ed
Spade <18681023+0xspade@users.noreply.github.com>
2023-10-13 05:41:24 +0000

    Update deploy.sh
    added discord webhook

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,9 @@
 read -p "Enter Slack Incoming Hook: " "slack_incoming_webhook"
 vercel secret add slack-incoming-webhook "$slack_incoming_webhook"
 
+read -p "Enter Discord Webhook: " "discord_webhook"
+vercel secret add discord-webhook "$discord_webhook"
+
 read -p "Enter IMGBB API Key: " "imgbb_api_key"
 vercel secret add imgbb-api-key "$imgbb_api_key"
 
